9ddcfc0907f00f4a3303e3d6b0736bb1f4a6bed7
Cheng Huang <cheng.huang2@exxonmobil.com>
2020-05-04 16:18:02 +0800

    [Cheng] #NA write implementation for Commodity

--- src/main/java/cn/xpbootcamp/gilded_rose/commodity/Commodity.java
+++ src/main/java/cn/xpbootcamp/gilded_rose/commodity/Commodity.java
@@ -3,6 +3,8 @@ package cn.xpbootcamp.gilded_rose.commodity;
 import lombok.Getter;
 import lombok.Setter;
 
+import static java.lang.Math.max;
+
 @Getter
 public class Commodity {
 
@@ -11,11 +13,26 @@ public class Commodity {
   @Setter
   private int sellIn;
 
-  public void setQuality(double quality) {
+  private static final double DEFAULT_QUALITY_DROPDOWN_PER_DAY = 1.0;
 
+  private static final double MAX_QUALITY = 50.0;
+  private static final double MIN_QUALITY = 0.0;
+
+  public void setQuality(double quality) {
+    if (quality > MAX_QUALITY || quality < MIN_QUALITY) {
+      throw new IllegalArgumentException(String.format("Quality must be between [{}, {}]", MIN_QUALITY, MAX_QUALITY));
+    }
+
+    this.quality = quality;
   }
 
   public void passDays(int i) {
+    sellIn -= i;
 
+    if (sellIn < 0) {
+      quality = max(0.0, quality - DEFAULT_QUALITY_DROPDOWN_PER_DAY * 2);
+    } else {
+      quality = max(0.0, quality - DEFAULT_QUALITY_DROPDOWN_PER_DAY * 1);
+    }
   }
 }
